150ee33003b1051170a285d5e4117fc9c08de9ff
Pieter du Plessis <pietertpt@gmail.com>
2026-06-26 22:28:19 +0200

    Add JParse exception file

---
+++ src/main/java/page/pieters/cenv/CenvJParseException.java
@@ -0,0 +1,12 @@
+package page.pieters.cenv;
+
+public class CenvJParseException extends Exception {
+
+    public CenvJParseException(String message) {
+        super(message);
+    }
+
+    public CenvJParseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
